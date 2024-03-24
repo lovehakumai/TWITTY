9916465849d250941523ca30862844a49cf442ff
lovehakumai <masaotomeza@gmail.com>
2024-03-24 11:38:08 +0900

    ログイン処理完了

--- sns_user.py
+++ sns_user.py
@@ -5,28 +5,48 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import models.Users as models_users
 import sqlite3
+import user_sqlite
+import bcrypt
+
+# アカウント作成処理
+def try_create_account(form):
+    user_id = form.get('user_id')
+    password = form.get('password')
+    # パスワードをハッシュ化
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    # データベースにユーザを追加
+    conn = sqlite3.connect(user_sqlite.USER_FILE)
+    c = conn.cursor()
+    c.execute('INSERT INTO users (user_id, password) VALUES(?,?)',(user_id, hashed_password))
+    conn.commit()
+    conn.close()
+    print(f"User {user_id} is created")
 
 # ログイン処理 - 入力されたIDとパスワードが一致しているかを確認する
 def try_login(form):
     user_id = form.get('user_id')
     password=form.get('password')
     
-    ok = check_user_match(user_id, password)
+    # ユーザー名に基づいてハッシュ化されたパスワードを取得
+    conn = sqlite3.connect(user_sqlite.USER_FILE)
+    c = conn.cursor()
+    c.execute('SELECT password FROM Users where user_id=?',(user_id,))
+    result = c.fetchone()
 
-    if not ok:
+    if result is None:
+        print('user is not found')
         return False
-    session['login'] = user_id
-    return True
-
-def check_user_match(user_id,password):
-    # Usersテーブルから入力されたIDに対するレコードを取得する
-    users_id_record = select('SELECT id,password FROM Users WHERE id=?',user_id)
-    if len(users_id_record)==0:
-        return None
-    ok = models_users.check_password_hash(users_id_record[1])
-    if not ok:
-        return None
-    return ok
+    else:
+        hashed_password = result[0]
+        # 入力されたパスワードがデータベースのハッシュと一致するかを検証
+        if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+            print('Login Sucessful')
+            session['login'] = user_id
+            return True
+        else:
+            print('Password is Incorrect')
+            return False
 
 
 # ログイン必須を処理するデコレーターを定義
--- user_sqlite.py
+++ user_sqlite.py
@@ -9,4 +9,29 @@ POST_COMMUNICATION_FILE = BASE_DIR + '/sqlite/Post_communications.sqlite3'
 
 
 def open_db():
-    conn = sqlite3.connect()
+    conn = sqlite3.connect(USER_FILE)
+    conn.row_factory = dict_factory
+    return conn
+
+# SWLWCT句の結果を辞書で得られるようにする
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+        print(d) # test
+    return d
+
+# SQLを実行する時
+def exec(sql, *args):
+    db = open_db()
+    c = db.cursor()
+    c.execute(sql, args)
+    db.commit()
+    return c.lastrowid
+
+# SQLを実行して結果を得る
+def select(sql, *args):
+    db = open_db()
+    c = db.cursor()
+    c.execute(sql, args)
+    return c.fetchall()
